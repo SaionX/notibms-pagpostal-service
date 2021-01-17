c3f632932e40b6fae64976af0d4de325be4230e3
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/pagador/postal/NotibmsPagpostalServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.pagador.postal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsPagpostalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsPagpostalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/pagador/postal/NotibmsPagpostalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.pagador.postal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsPagpostalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
